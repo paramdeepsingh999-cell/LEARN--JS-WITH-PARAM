8057c2f8144b4cfd384f7f4d1d6c757d76b43e44
paramdeepsingh999-cell <paramdeepsingh999@gmail.com>
2025-10-04 06:58:48 +0000

    JS Data Type

--- basics/02_dataTypes.js
+++ basics/02_dataTypes.js
@@ -1,2 +1,34 @@
 "use strict"; // treat all JS code as newer version
 // alert(3+3) // we are using nodejs. not browser
+
+console.log(3 
+    + 
+    3) // code reusability should be high
+
+console.log("PARAM")
+
+/* documentation Link
+   https://tc39.es/ecma262/#sec-intro
+*/
+
+let name = "Param" // string data type
+let age = 39 // number data type
+let isLoggedIn = false // boolean data type
+
+// Range of DATA TYPES
+
+// PREMITIVE DATA TYPES
+// number ==> 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null // its standalone value  example: let state = null
+// undefined => example let state;
+// symbol  uniqueness of components
+
+// object
+
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
+
