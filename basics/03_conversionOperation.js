544c96ad9d2380fe2c5d5a8312ffb9f570ed68d9
paramdeepsingh999-cell <paramdeepsingh999@gmail.com>
2025-10-04 08:02:51 +0000

    conversion and operations

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,128 @@
+/* first case
+
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+*/
+
+
+/*second case
+
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+*/
+
+
+/* Third case
+
+let score = "33"
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+*/
+
+
+/* Fourth case
+
+let score = "33abc"
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+*/
+
+
+
+/* FIFTH case
+
+let score = null
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+*/
+
+
+/* SIXTH Case
+
+let score = undefined
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+*/
+
+/* SEVENTH Case
+
+let score = true
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+*/
+
+
+// EIGHTH Case
+
+let score = "PARAM"
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+/* Notes :
+
+"33" => 33
+33abc => NaN
+TRUE => 1; false => 0;
+
+*/
+
+/* Ninth Case
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+*/
+
+/* Tenth Case
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+*/
+
+/* Eleventh Case
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+*/
+
+
+
+
+
+
