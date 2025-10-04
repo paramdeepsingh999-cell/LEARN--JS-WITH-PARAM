a8b09dcd46623dd31321fb22b55e75467d24b951
paramdeepsingh999-cell <paramdeepsingh999@gmail.com>
2025-10-04 10:27:56 +0000

    Why string to number conversion is confusing

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -142,10 +142,59 @@ console.log(typeof booleanIsLoggedIn)
 
 // "hitesh" => true
 
+/*
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber);
+*/
+
+// ***************** OPERATIONS ********************* 
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+/*  console.log(2+2);
+    console.log(2-2);
+    console.log(2*2);
+    console.log(2/2);
+    console.log(2**2);
+    console.log(2%2);
+*/
+
+let str1 = "Hello"
+let str2 = " Param"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+//console.log("1" + 2)
+//console.log(1 + "2")
+
+// ToPrimitive 
+
+// console.log("1" + 2 + 2) // First string conver all to string
+// console.log(1 + 2 + "2") // if first value numeric then its numeric untill string occur
+
+// LINK TO STUDY  
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+/*
+let gamescore = 100
+++gamescore;
+console.log(gamescore);
+*/
+
+/*
+let gamescore = 100
+gamescore++;
+console.log(gamescore);
+*/
+
+
+
+
 
 
 
