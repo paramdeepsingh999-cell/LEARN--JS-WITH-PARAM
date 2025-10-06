081c11889d5a1ac8ced98b808d359ceb7f0631f1
paramdeepsingh999-cell <paramdeepsingh999@gmail.com>
2025-10-06 12:11:19 +0000

    Dates and Time

---
+++ basics/07_dates_in_js.js
@@ -0,0 +1,32 @@
+// Dates
+
+let myDate = new Date()
+    console.log(myDate);
+
+// let's convert date into String
+
+console.log(myDate.toString());
+console.log(typeof myDate);
+
+console.log(myDate.toDateString()); // Mon Oct 06 2025
+console.log(myDate.toISOString()); // 2025-10-06T11:52:51.503Z
+console.log(myDate.toJSON()); // 2025-10-06T11:52:51.503Z
+console.log(myDate.toLocaleDateString()); // 10/6/2025
+console.log(myDate.toLocaleString()); // 10/6/2025, 11:52:51 AM
+console.log(myDate.toLocaleTimeString()); // 11:52:51 AM
+console.log(myDate.toTimeString()); // 11:52:51 GMT+0000 (Coordinated Universal Time)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
