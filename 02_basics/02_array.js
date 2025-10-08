91390e4ff0b3ac741a22af8d257dfb5f3bb3c059
paramdeepsingh999-cell <paramdeepsingh999@gmail.com>
2025-10-08 11:35:20 +0000

    array method

---
+++ 02_basics/02_array.js
@@ -0,0 +1,43 @@
+const marvel_heroes = ["thor", "ironman", "spiderman"]
+
+const dc_heroes = ["superman", "flash", "batman"]
+//marvel_heroes.push(dc_heroes)
+//console.log(marvel_heroes);
+//console.log(marvel_heroes[3][1]);
+
+// const allHeroes = marvel_heroes.concat(dc_heroes)
+// console.log(allHeroes);
+
+//Spread Operator
+
+// const allNewHeroes = [...marvel_heroes, ...dc_heroes]
+// console.log(allNewHeroes);
+
+// const anotherArray = [ 1, 2, 3, [4,5,6], 7, [6,7, [4,5]]]
+// const myreal_Another_array = anotherArray.flat(Infinity)
+// console.log(myreal_Another_array);
+
+
+//console.log(Array.isArray("Paramdeep"))
+//console.log(Array.from("Param"));
+
+// console.log(Array.from({name: "Param"})); // you should tell him to make array from key or values, otherwise it returen empty array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+
+console.log(Array.of(score1, score2, score3));
+
+
+
+
+
+
+
+
+
+
+
+
