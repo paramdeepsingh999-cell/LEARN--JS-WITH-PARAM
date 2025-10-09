63d514007677f3672e9c04c4a38f7d6253bb934f
paramdeepsingh999-cell <paramdeepsingh999@gmail.com>
2025-10-09 11:07:53 +0000

    javascript objects acess

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -9,9 +9,14 @@
 // Object.create ==> its constructor approach to declare an object
 
 // object literal
+
+const mySym = Symbol("key1")
 const jsUser = {
     name: "paramdeep", // key and value pair method
     "full name": "Paramdeep Singh Thakur",
+    [mySym]: "myKey1",
+    // its the write syntex to declare symbol under object
+    // mySym: "myKey1", // its not the right way to use symbol in object
     age: 39,
     location: "Punjab",
     email: "param@yahoo.work",
@@ -22,12 +27,39 @@ const jsUser = {
 //how to get acces of objects
 // mostly people use jsUser.email apraoch to acces object , but htere is one another way to do that , lets discuss
 
-console.log(jsUser.email);
+//console.log(jsUser.email);
 // console.log(jsUser[email]);
-console.log(jsUser["email"]);
+//console.log(jsUser["email"]);
 
 // console.log(jsUser."full name"); You can not acces that key:value with (.) or ("") methods.
-console.log(jsUser["full name"]);
+//console.log(jsUser["full name"]);
+//console.log(jsUser.mySym); Here it will return exact key value
+//console.log(typeof jsUser.mySym); But data type is not symbol its string here
+
+//console.log(jsUser[mySym]);
+//console.log(typeof jsUser[mySym]);
+
+//jsUser.email= "param@goglu.work"
+//Object.freeze(jsUser) // you can use freeze to lock object and nobody can change values of that object
+//jsUser.email= "param@goglu.work786"
+//console.log(jsUser);
+
+// jsUser.greeting = function(){
+//     console.log("Hello JS USER");    
+// }
+
+// console.log(jsUser.greeting());
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello JS USER, ${this.name}}`);    
+}
+
+console.log(jsUser.greetingTwo());
+
+
+
+
+
 
 
 
