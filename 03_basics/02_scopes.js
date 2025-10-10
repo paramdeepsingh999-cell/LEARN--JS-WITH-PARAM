5214155352c3505a1a45ad4b0b28db4edf935d90
paramdeepsingh999-cell <paramdeepsingh999@gmail.com>
2025-10-10 12:15:26 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+{} //function, if else have same called scope.  not in object there its for object declaration
+
+if (true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
