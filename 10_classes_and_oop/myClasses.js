d701e191993eafc16506027a95d262c5e0b5eb36
paramdeepsingh999-cell <paramdeepsingh999@gmail.com>
2025-11-01 12:58:12 +0000

    static prop and inheritance

--- 10_classes_and_oop/myClasses.js
+++ 10_classes_and_oop/myClasses.js
@@ -1,16 +1,45 @@
 //ES6
 
 
-class User {
-    constructor(username, email, password){
+// class User {
+//     constructor(username, email, password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+    
+//    encryptPassword(){
+//     return  `${this.password}abc`
+//    }
+
+//    changeUsername(){
+//     return `${this.username.toUpperCase()}`
+//    }
+// }
+
+// const chai = new User("chai", "chai@fkbook.com", "12345")
+// console.log(chai.encryptPassword());
+// console.log(chai.changeUsername());
+
+
+//behind the scene
+
+function User(username, email, password){
         this.username = username;
-        TouchList.email = email;
+        this.email = email;
         this.password = password;
     }
-    encryptPassword(){
-        return  '${this.password}abc'
-    }
-}
 
-const chai = new User("chai", "chai@fkbook.com", "12345")
-console.log(chai.encryptPassword());
+    User.prototype. encryptPassword = function(){
+    return  `${this.password}abc`
+   }
+
+     User.prototype.changeUsername= function(){
+    return `${this.username.toUpperCase()}`
+   }
+
+    const tea = new User("tea", "tea@fkbook.com", "12345")
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
+
+
